e3dc52dffce4bace30dda91d82b7a5b207b8b8c0
tpereira <tpereira@student.42lisboa.com>
2023-03-17 19:02:47 +0000

    finished ex00

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,16 +6,22 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:12:50 by tpereira          #+#    #+#             */
-/*   Updated: 2023/03/17 12:13:33 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/03/17 18:56:53 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include "../includes/Zombie.hpp"
 
 int	main(void)
 {
+	Zombie foo;
+	Zombie *bar = foo.newZombie("Bar");
 	
+	bar->randomChump("Chump");
+	foo.announce();
+	bar->announce();
 
+	delete bar;
 
 	return (0);
 }
