c75bcc95e21ee0bd5793d8163b2bad235ceeb9fc
Tiago Pereira <tpereira@c2r1s6.42lisboa.com>
2023-03-21 14:51:31 +0000

    save

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 22:44:25 by tpereira          #+#    #+#             */
-/*   Updated: 2023/03/20 22:22:28 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/03/21 14:49:06 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,41 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc >= 2)
+	if (argc == 2)
 	{
-		std::ifstream 		file;
-		std::stringstream	replace;
-		file.open(argv[1]);
-		replace << file.rdbuf();
-		file.close();
-		std::string str = replace.str();
-		
-		if (str.compare(argv[2]))
-			std::cout << "exists \n";
+		char *fileContent;
+		std::ifstream file(argv[1]);
+		if (file)
+		{
+			std::streambuf *buffer = file.rdbuf();
+			std::streamsize size = buffer->pubseekoff(0, file.end);
+			buffer->pubseekoff(0, file.beg);
+			fileContent = new char[size];
+			buffer->sgetn(fileContent, size);
+			file.close();
+			std::cout.write(fileContent, size);
+			delete[] fileContent;
+		}
 
-		std::cout << str << "\n";
+		// std::stringbuf buffer;
+		// std::ostream os(&buffer);
+
+		// buffer.sputn(argv[2], argv[2].str);
+		// // os << std::hex << 255;
+		// std::cout << buffer.str();
+
+		// std::ifstream file;
+		// std::stringstream	replace;
+		// file.open(argv[1]);
+		// replace << file.rdbuf();
+		// file.close();
+		// std::string str = replace.str();
+		
+
+		// 	std::cout << str << "\n";
 	}
 	else
-		std::cout << "Error: too few arguments\n";
+		std::cout << "Usage: ./sed_is_for_losers <filename> <s1> <s2>\n";
 
 	return (0);
 }
