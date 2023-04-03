4ff0b3ccefb5d1fdf8dcfb34d4a5594803d8e7bd
Tiago Pereira <tpereira@c2r4s6.42lisboa.com>
2023-04-03 10:50:41 +0100

    working, but can't iterate the strings like C (char by char)

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 22:44:25 by tpereira          #+#    #+#             */
-/*   Updated: 2023/04/03 10:12:51 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/04/03 10:42:43 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ std::string getFileContent(char **argv)
 std::string replaceString(std::string content, std::string s1, std::string s2, std::string filename)
 {
 	std::string newContent;
-	int			tmp;
+	size_t		tmp;
 	int			start;
 	int			end;
 	int			j;
@@ -34,20 +34,25 @@ std::string replaceString(std::string content, std::string s1, std::string s2, s
 	tmp = 0;
 	if (content.find(s1) != std::string::npos)
 	{
-		start = content.find(s1[j]);
-		tmp = start;
-		while (content[start] == s1[j])
+		while (content.find(s1) != std::string::npos)
 		{
-			j++;
-			start++;
-		}
-		start = tmp;
-		end = j + tmp;
-		while (end > start)
-		{
-			content[start] = s2[s1.length() - j];
-			start++;
-			j--;
+			start = content.find(s1[j]);
+			tmp = start;
+			while (content[start] == s1[j])
+			{
+				j++;
+				start++;
+			}
+			start = tmp;
+			end = j + tmp;
+			while (end > start)
+			{
+				content[start] = s2[s1.length() - j];
+				start++;
+				j--;
+			}
+			if (tmp == content.size())
+				break ;
 		}
 	}
 	else
@@ -59,15 +64,19 @@ int	main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		std::string s1 = argv[2];
-		std::string s2 = argv[3];
-		std::string fileContent;
-		std::string type = ".replace";
-		std::string replaceFileName = argv[1] + type;
-		std::ofstream replaceFile(replaceFileName.c_str());
+		std::ifstream	file;
+		std::string 	s1 = argv[2];
+		std::string 	s2 = argv[3];
+		std::string 	fileContent;
+		std::string 	type = ".replace";
+		std::string 	replaceFileName = argv[1] + type;
+		std::ofstream	replaceFile(replaceFileName.c_str());
+		file.open(argv[1]);
 		fileContent = getFileContent(argv);
-		replaceFile << replaceString(fileContent, s1, s2, replaceFileName);
-		// std::cout << fileContent;
+		if (file)
+			replaceFile << replaceString(fileContent, s1, s2, argv[1]);
+		else
+			std::cout << "error: file doesn't exist or you don't have the right permissions\n";
 	}
 	else
 		std::cout << "Usage: ./sed_is_for_losers <filename> <s1> <s2>\n";
